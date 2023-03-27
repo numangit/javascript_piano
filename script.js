2c8c2f95274abba083dcf94dcc1d98fe0e98493f
numangit <abdulnumanjob@gmail.com>
2023-03-28 01:43:35 +0600

    function added ot play key notes

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 //select element
 const pianoKeysEl = document.querySelectorAll(".piano-keys .key");
 
+//audio file location
+let audio = new Audio("/tunes/a.wav");
+
+//function to play tune
+const playTune = (key) => {
+    audio.src = `tunes/${key}.wav`;
+    audio.play();
+}
+
 pianoKeysEl.forEach(key => {
     key.addEventListener("click", () => playTune(key.dataset.key))
 })
