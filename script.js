1675590fddf740e0c12c6f2e5b50ec23df55e145
numangit <abdulnumanjob@gmail.com>
2023-03-28 01:37:46 +0600

    code to get element on key press added

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hi");
+//select element
+const pianoKeysEl = document.querySelectorAll(".piano-keys .key");
+
+pianoKeysEl.forEach(key => {
+    key.addEventListener("click", () => playTune(key.dataset.key))
+})
